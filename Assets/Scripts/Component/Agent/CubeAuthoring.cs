f230dca9464e845a9ff54780b11daa1be32d3b57
kobayashi5829 <jimonjitou5884@gmail.com>
2025-04-08 13:45:56 +0900

    push

---
+++ Assets/Scripts/Component/Agent/CubeAuthoring.cs
@@ -0,0 +1,20 @@
+using Unity.Entities;
+using UnityEngine;
+
+namespace Agent
+{
+    public struct Cube : IComponentData { }
+
+    [DisallowMultipleComponent]
+    public class CubeAuthoring : MonoBehaviour
+    {
+        class Baker : Baker<CubeAuthoring>
+        {
+            public override void Bake(CubeAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent<Cube>(entity);
+            }
+        }
+    }
+}
