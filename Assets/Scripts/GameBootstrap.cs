54df0a772513ffe8b77773c4c1158189a30f2f5c
kobayashi5829 <jimonjitou5884@gmail.com>
2025-04-11 10:52:55 +0900

    push

--- Assets/Scripts/GameBootstrap.cs
+++ Assets/Scripts/GameBootstrap.cs
@@ -2,29 +2,38 @@ using UnityEngine;
 using Unity.NetCode;
 using Unity.Networking.Transport;
 using Unity.Entities;
+using System.Linq;
 
 [UnityEngine.Scripting.Preserve]
 public class GameBootstrap : ClientServerBootstrap
 {
+    private static readonly ushort PORT = 53647;
+
     public override bool Initialize(string defaultWorldName)
     {
 #if UNITY_EDITOR
-        AutoConnectPort = 53647;
+        AutoConnectPort = PORT;
         CreateDefaultClientServerWorlds();
         Debug.Log("Editor");
-#elif SERVER_BUILD
-        var serverWorld = CreateServerWorld(defaultWorldName);
-        var listenRequest = serverWorld.EntityManager.CreateEntity(typeof(NetworkStreamRequestListen));
-        var endpoint = NetworkEndpoint.Parse("0.0.0.0", 53647);
-        serverWorld.EntityManager.SetComponentData(listenRequest, new NetworkStreamRequestListen { Endpoint = endpoint });
-        Debug.Log("Server");
-#else
-        var clientWorld = CreateClientWorld(defaultWorldName);
-        var connectRequest = clientWorld.EntityManager.CreateEntity(typeof(NetworkStreamRequestConnect));
-        var endpoint = NetworkEndpoint.Parse("127.0.0.1", 53647);
-        clientWorld.EntityManager.SetComponentData(connectRequest, new NetworkStreamRequestConnect { Endpoint = endpoint });
-        Debug.Log("Client");
-#endif
         return true;
+#endif
+        if (System.Environment.GetCommandLineArgs().Contains("-server"))
+        {
+            var serverWorld = CreateServerWorld(defaultWorldName);
+            var listenRequest = serverWorld.EntityManager.CreateEntity(typeof(NetworkStreamRequestListen));
+            var endpoint = NetworkEndpoint.AnyIpv4.WithPort(PORT);
+            serverWorld.EntityManager.SetComponentData(listenRequest, new NetworkStreamRequestListen { Endpoint = endpoint });
+            Debug.Log("Server");
+            return true;
+        }
+        else
+        {
+            var clientWorld = CreateClientWorld(defaultWorldName);
+            var connectRequest = clientWorld.EntityManager.CreateEntity(typeof(NetworkStreamRequestConnect));
+            var endpoint = NetworkEndpoint.Parse("34.83.229.95", PORT);
+            clientWorld.EntityManager.SetComponentData(connectRequest, new NetworkStreamRequestConnect { Endpoint = endpoint });
+            Debug.Log("Client");
+            return true;
+        }
     }
 }
